748dfad10a160e2b9719427973e12690b6f4563a
Selas23 <[Selas.PBiju@ust.com]>
2023-06-17 23:49:04 +0530

    momdified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  user: any = {
+    username: '',
+    password: ''
+  };
+  showPassword: boolean = false;
 
+  onSubmit(loginForm: NgForm) {
+    if (loginForm.valid) {
+      // Perform login logic
+      // You can add your own implementation here, such as calling an API for authentication
+
+      // Reset the form
+      loginForm.resetForm();
+
+      // Clear the user object
+      this.user = {
+        username: '',
+        password: ''
+      };
+    }
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
 }
